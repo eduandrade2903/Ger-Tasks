c80731f20bcaa5ed92648cfafd9855cc67167022
Eduardo <eduandrade.lara@hotmail.com>
2025-05-22 18:47:14 -0300

    Primeiro commit

---
+++ src/test/java/ger_task/v1/V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ger_task.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
