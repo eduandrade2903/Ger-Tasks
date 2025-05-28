6800ff9a79c16c41e50354b50dcc7d26452a41de
Eduardo <eduandrade.lara@hotmail.com>
2025-05-27 21:58:29 -0300

    primeiro commit

---
+++ Ger_taskBack/src/main/java/ger_taskv1/GerTaskv1Application.java
@@ -0,0 +1,13 @@
+package ger_taskv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerTaskv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerTaskv1Application.class, args);
+	}
+
+}
---
+++ Ger_taskBack/src/test/java/ger_taskv1/GerTaskv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ger_taskv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerTaskv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
